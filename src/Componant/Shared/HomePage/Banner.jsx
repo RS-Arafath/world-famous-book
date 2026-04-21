8cda56064f21ee2fe3f9298bb1c3f193f0a401a5
Arafath <rsarafath.signin@gmail.com>
2026-04-22 00:13:06 +0600

    Refactor code structure for improved readability and maintainability

--- src/Componant/Shared/HomePage/Banner.jsx
+++ src/Componant/Shared/HomePage/Banner.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen container mx-auto bg-[#F3F3F3">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div className="max-w-md text-left">
-            <h1 className="text-5xl font-bold">
-              Books to freshen up your bookshelf!
-            </h1>
+    <div className=" container my-10 hero flex items-center bg-base-200 min-h-[70vh]  mx-auto rounded-2xl shadow-2xl">
+      <div className="hero-content flex-col top-0 lg:flex-row-reverse w-full justify-between items-center  ">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div className="text-left space-y-5">
+          <h1 className="text-5xl font-bold">
+            Books to freshen <br /> up your bookshelf!
+          </h1>
 
-            <button className="btn btn-success">View The List</button>
-          </div>
+          <button className="btn btn-success">View The List</button>
         </div>
       </div>
     </div>
