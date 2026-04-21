daa51bd1f8a5f4049f1dfb9661564a6b251c5470
Arafath <rsarafath.signin@gmail.com>
2026-04-22 00:28:42 +0600

    feat: update Banner component to use local image and enhance styling; modify index.html for theme consistency

--- src/Componant/Shared/HomePage/Banner.jsx
+++ src/Componant/Shared/HomePage/Banner.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import BannerImg from '../../../assets/pngwing 1.png'
 const Banner = () => {
   return (
     <div className=" container my-10 hero flex items-center bg-base-200 min-h-[70vh]  mx-auto rounded-2xl shadow-2xl">
-      <div className="hero-content flex-col top-0 lg:flex-row-reverse w-full justify-between items-center  ">
+      <div className="hero-content flex-col top-0 lg:flex-row-reverse w-full justify-between items-center mx-15 ">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
+          src={BannerImg}
+          className="rotate-5"
         />
         <div className="text-left space-y-5">
           <h1 className="text-5xl font-bold">
