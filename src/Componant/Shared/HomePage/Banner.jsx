27ad6c33e7af9e0a013781d085d969a0b531b8e8
Arafath <rsarafath.signin@gmail.com>
2026-04-22 11:11:15 +0600

    feat: update Banner and Navbar components with improved styling and layout adjustments

--- src/Componant/Shared/HomePage/Banner.jsx
+++ src/Componant/Shared/HomePage/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import BannerImg from '../../../assets/pngwing 1.png';
 const Banner = () => {
   return (
-    <div className=" container my-10 hero flex items-center bg-base-200 min-h-[70vh]  mx-auto rounded-2xl shadow">
-      <div className="hero-content flex-col top-0 lg:flex-row-reverse w-full justify-between items-center mx-15 ">
+    <div className=" container font-primary hero flex items-center bg-base-200 h-175 sm:h-200 py-7 lg:h-107.5 max-h-[75vh] mx-auto rounded-2xl shadow my-20 lg:my-25 ">
+      <div className="hero-content mx-auto flex-col top-0 lg:flex-row-reverse w-full justify-between items-center ">
         <div className="hover-3d">
           {/* content */}
           <figure className="max-w-70 rounded-2xl">
@@ -19,13 +19,15 @@ const Banner = () => {
           <div></div>
           <div></div>
         </div>
-        <div className="text-left my-9 space-y-5">
-          <h1 className="text-3xl sm:text-4xl md:text-fxl font-bold ">
+        <div className="text-left my-9 space-y-5 sm:space-y-6 md:space-y-9">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold ">
             Books to freshen <br className="hidden lg:flex" /> up your
             bookshelf!
           </h1>
 
-          <button className="btn btn-success text-white/80">View The List</button>
+          <button className="bg-amber-500 font-bold cursor-pointer hover:bg-amber-600 transition-all text-sm sm:text-lg md:text-xl duration-300 transform hover:scale-105 px-4 sm:px-6 md:px-8 py-3 rounded-lg  shadow-lg text-white">
+            View The List
+          </button>
         </div>
       </div>
     </div>
--- src/Componant/Shared/NavBar/Navbar.jsx
+++ src/Componant/Shared/NavBar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <NavLink
           to={'/'}
           className={({ isActive }) =>
-            `font-semibold ${isActive ? 'text-green-500 border border-green-400' : ''}`
+            `font-semibold mr-1 ${isActive ? 'text-green-500 border border-green-400' : ''}`
           }
         >
           Home
@@ -19,7 +19,7 @@ const Navbar = () => {
         <NavLink
           to={'/books'}
           className={({ isActive }) =>
-            `font-semibold ${isActive ? 'text-green-500 border border-green-400' : ''}`
+            `font-semibold mr-1 ${isActive ? 'text-green-500 border border-green-400' : ''}`
           }
         >
           Listed Books
@@ -38,58 +38,60 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="container mx-auto navbar bg-base-100 shadow-sm font-secondary">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <nav className="left-0 top-0  right-0 shadow-sm font-secondary fixed z-50 backdrop-blur-md bg-white/10 border border-white/20  ">
+      <div className="container mx-auto navbar  ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {' '}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{' '}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100/20 backdrop-blur-xl border border-white/10 rounded-box z-10 mt-3 w-30 p-2 shadow-lg"
             >
-              {' '}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{' '}
-            </svg>
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100/20 backdrop-blur-xl border border-white/10 rounded-box z-10 mt-3 w-30 p-2 shadow-lg"
+          <a
+            onClick={() => (window.location.href = '/')}
+            className="font-bold cursor-pointer text-sm sm:text-xl md:text-3xl"
           >
-            {links}
-          </ul>
-        </div>
-        <a
-          onClick={() => (window.location.href = '/')}
-          className="font-bold cursor-pointer text-sm sm:text-xl"
-        >
-          Book Vibe
-        </a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <div className="navbar-end flex gap-3">
-        <NavLink to={'/signIn'}>
-          {' '}
-          <a className="btn bg-green-600 p-2 font-semibold md:p-4 hover:bg-green-500">
-            Sign In
+            Book Vibe
           </a>
-        </NavLink>
-        <NavLink to={'/signUp'}>
-          {' '}
-          <button className="btn font-secondary btn-outline p-2 md:p-4 btn-info">
-            Sign Up
-          </button>
-        </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end flex gap-3">
+          <NavLink to={'/signIn'}>
+            {' '}
+            <a className="btn bg-green-600 p-2 font-semibold md:p-4 hover:bg-green-500">
+              Sign In
+            </a>
+          </NavLink>
+          <NavLink to={'/signUp'}>
+            {' '}
+            <button className="btn font-secondary btn-outline p-2 md:p-4 btn-info">
+              Sign Up
+            </button>
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
