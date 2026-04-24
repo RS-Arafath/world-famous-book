9260d4e3e9624ae7cba4272253cfc580647d8092
Arafath <rsarafath.signin@gmail.com>
2026-04-24 22:25:52 +0600

    fix: add react-tabs dependency and import in Books component

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { BookContext } from '../../BookProvider/BookProvider';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 const Books = () => {
     const { handlemarkAsRead, storeBook, handleWishList, wishlist } =
       useContext(BookContext);
@@ -11,6 +11,7 @@ const Books = () => {
     <div className=" w-11/12 mx-auto border  mt-20 mb-10 md:mb-15 lg:mb-20 lg:my-25">
       <h2> Read list: {storeBook.length}</h2>
       <h3> wish list: {wishlist.length}</h3>
+     
     </div>
   );
 };
